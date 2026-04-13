e6005eb49a064ef4a4d0f405211906d7ee2c7c8b
Silas Flugum <siclops.ch@gmail.com>
2026-04-12 20:28:55 -0400

    Fix API URL for production

--- loan-precheck-frontend/src/services/api.js
+++ loan-precheck-frontend/src/services/api.js
@@ -1,12 +1,13 @@
 export const submitPrecheck = async (creditScore, income) => {
-  const res = await fetch("http://localhost:8080/precheck", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ creditScore, income })
-  });
+  const res = await fetch(
+    `${process.env.REACT_APP_API_URL}/precheck`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ creditScore, income })
+    }
+  );
 
   const data = await res.json().catch(() => null);
-
-  // ALWAYS return backend response as-is
   return data;
 };
