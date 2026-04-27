d713fba77c150bdbd7916026c27dd4cdd3b74051
Silas Flugum <siclops.ch@gmail.com>
2026-04-27 09:33:33 -0400

    fix: CORS alignment

--- loan-precheck-backend/src/main/java/io/github/sflugum/applicantprecheck/controller/CorsConfig.java
+++ loan-precheck-backend/src/main/java/io/github/sflugum/applicantprecheck/controller/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-	@Value("${app.cors.allowed-origins:http://localhost:3000}")
+	@Value("${app.cors.allowed-origin:http://localhost:3000}")
 	private String allowedOrigins;
 
 	@Override
