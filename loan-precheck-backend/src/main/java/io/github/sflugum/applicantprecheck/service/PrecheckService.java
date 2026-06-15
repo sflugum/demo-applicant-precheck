1165acf239da85174330a7ec0c5491f3c555ffcf
Silas Flugum <siclops.ch@gmail.com>
2026-06-14 22:21:29 -0400

    feat: update PrecheckService to connect database. PrecheckService test also updated

--- loan-precheck-backend/src/main/java/io/github/sflugum/applicantprecheck/service/PrecheckService.java
+++ loan-precheck-backend/src/main/java/io/github/sflugum/applicantprecheck/service/PrecheckService.java
@@ -1,26 +1,44 @@
 package io.github.sflugum.applicantprecheck.service;
 
+
+import io.github.sflugum.applicantprecheck.model.Applicant;
+import io.github.sflugum.applicantprecheck.repository.ApplicantRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PrecheckService {
+
+	private final ApplicantRepository repository;
+
+	public PrecheckService(ApplicantRepository repository) {
+		this.repository = repository;
+	}
+
 	public String evaluateApplicant(int creditScore, int income) {
+
+		String status;
 
 		// Inquiry validation
 		if (creditScore < 300 || creditScore > 850 || income < 0) {
-			return "REVIEW";
+			status = "REVIEW";
 		}
 
 		// Anti-fraud/High-income review check
 		else if (creditScore > 750 && income > 3000000) {
-			return "REVIEW";
+			status = "REVIEW";
 		}
 
 		// Standard approval
 		else if (creditScore > 650 && income > 40000) {
-			return "APPROVED";
+			status = "APPROVED";
 		} else {
-			return "REVIEW";
+			status = "REVIEW";
 		}
+
+		Applicant applicant = new Applicant(creditScore, income, "DEFAULT_MERCHANT", status);
+
+		repository.save(applicant);
+
+		return status;
 	}
 }
--- loan-precheck-backend/src/test/java/io/github/sflugum/applicantprecheck/service/PrecheckServiceTest.java
+++ loan-precheck-backend/src/test/java/io/github/sflugum/applicantprecheck/service/PrecheckServiceTest.java
@@ -1,23 +1,39 @@
 package io.github.sflugum.applicantprecheck.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+import io.github.sflugum.applicantprecheck.model.Applicant;
+import io.github.sflugum.applicantprecheck.repository.ApplicantRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-
+@ExtendWith(MockitoExtension.class)
 public class PrecheckServiceTest {
-	
-	private PrecheckService service = new PrecheckService();
+
+	@Mock
+	private ApplicantRepository repository;
+
+	@InjectMocks
+	private PrecheckService service;
 	
 	@Test
 	public void testHighCreditHighIncome_ShouldBeApproved() {
 		String result = service.evaluateApplicant(750, 50000);
 		assertEquals("APPROVED", result);
+
+		verify(repository).save(any(Applicant.class));
 	}
 	
 	@Test
 	public void testLowCredit_ShouldBeReview() {
 		String result = service.evaluateApplicant(300, 50000);
 		assertEquals("REVIEW", result);
-	}
 
+		verify(repository).save(any(Applicant.class));
+	}
 }
